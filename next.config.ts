4c4c58a878dc3a44fa52f8aff2eaef3c18b74a47
sujiu <2994338151@qq.com>
2025-11-02 16:46:37 +0800

    feat: 添加 Dockerfile 和 docker-compose.yml，配置数据库迁移和主应用服务，更新 .gitignore，调整构建脚本以支持 Prisma

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   // 指定输出文件追踪的根目录，避免 Next.js 误把上级目录的其他 lockfile 当作工作区根目录
   // 参考：https://nextjs.org/docs/app/api-reference/config/next-config-js/output#caveats
   outputFileTracingRoot: path.join(__dirname),
+  output: "standalone",
 };
 
 export default nextConfig;
